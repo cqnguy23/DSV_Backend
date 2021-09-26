13652c29d4635278497354f9137bfb4a4b358dab
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-09-26 17:35:02 +0700

    fix heroku bugs

---
+++ models/User.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import env from "dotenv";
+import bcrypt from "bcrypt";
+
+env.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+const ADMIN_SECRET_KEY = process.env.ADMIN_SECRET_KEY;
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  email: String,
+  name: String,
+  balance: { type: Number, default: 0 },
+  profileImg: String,
+  password: String,
+  role: {
+    type: String,
+    enum: ["seller", "customer"],
+    default: "customer",
+  },
+});
+//generatePwd
+userSchema.post("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  const encodedPwd = await bcrypt.hash(this.password, salt);
+
+  await User.findByIdAndUpdate(this, {
+    password: encodedPwd,
+  });
+});
+
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ id: this._id, role: this.role }, SECRET_KEY, {
+    expiresIn: "24h",
+  });
+  return token;
+};
+userSchema.methods.generateAdminToken = function () {
+  const token = jwt.sign({ id: this._id, role: this.role }, ADMIN_SECRET_KEY, {
+    expiresIn: "5h",
+  });
+  return token;
+};
+const User = mongoose.model("User", userSchema);
+
+export default User;
